e037526400515f604350eaf756b62fe07f166857
air.lee <upwardlwy@sina.com>
2017-02-24 14:26:17 +0800

    1111

---
+++ PluginH5/js/main.js
@@ -0,0 +1,54 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 16-12-29
+ * Time: 下午3:36
+ * To change this template use File | Settings | File Templates.
+ */
+$(function(){
+    /**
+     * fullPage
+     */
+    $("#fullPage").fullpage({
+        continuousVertical: true,
+        anchors: ['firstPage', 'secondPage', '3rdPage'],
+        menu: '#menu',
+        navigation: true,
+        afterLoad: function(anchorLink,index){
+            $(".section2 img").hide().removeClass("");
+
+            if(index == 2){
+                $(".section2 .section2-img").show().addClass("section2-fadeInUp");
+                $(".section2 .section2-img2").show().addClass("section2-fadeInUp2");
+                $(".section2 .section2-img3").show().addClass("section2-fadeInUp3");
+            }
+        }
+    });
+
+    $.fn.fullpage.setAutoScrolling(true);
+
+    /**
+     *  music
+     */
+    $("#music").click(function(){
+        if($("audio")[0].paused){
+            $("audio")[0].play();
+            $(this).removeClass("stopped");
+
+        }else {
+            $("audio")[0].pause();
+            $(this).addClass("stopped");
+        }
+    });
+
+    //ios 浏览器打开 音频播放
+    $(document).one("touchstart", function() {
+        $("audio")[0].play();
+    });
+
+    /* //ios 微信中音频自动播放 注：结合Weixin JSAPI的WeixinJSBridgeReady才能生效
+     document.addEventListener("WeixinJSBridgeReady", function () {
+     $("audio")[0].play();
+     }, false);  */
+
+});
